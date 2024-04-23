21176fa531fd2428fc2c83cb87c5a014ad3eee61
Matthias Volk <mv@infrablue.org>
2024-04-23 18:16:21 +0200

    Refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
-use ndarray::{arr1, array, Array1};
+use ndarray::{array, Array, Array1};
 
-fn ham_eom_1d_harmonic_oscillator(_t: f32, pq: &Array1<f32>) -> Array1<f32> {
+struct IntegrationParams {
+    step_size: f32,
+    t0: f32,
+    tmax: f32,
+}
+
+fn ham_eom_1d_harmonic_oscillator(_t: f32, pq: &Array1<f32>, dest: &mut Array1<f32>) {
     // H = p^2 / (2 * m) + k * q^2 / 2
     // \partial_q H = k * q
     // \partial_p H = p / m
     let m: f32 = 1.0;
     let k: f32 = 0.5;
 
-    array![-k * pq[1], pq[0] / m]
+    // array![-k * pq[1], pq[0] / m]
+    dest[0] = -k * pq[1];
+    dest[1] = pq[0] / m;
 }
 
-fn runge_kutta(h: f32, t0: f32, pq0: Array1<f32>, tmax: f32) {
+fn runge_kutta(params: IntegrationParams, pq0: Array1<f32>) {
+    let h = params.step_size;
+    let tmax = params.tmax;
     let mut pq = pq0;
-    let mut t = t0;
+    let mut t = params.t0;
 
-    let mut k1: Array1<f32>;
-    let mut k2: Array1<f32>;
-    let mut k3: Array1<f32>;
-    let mut k4: Array1<f32>;
+    let mut k1 = Array::zeros(pq.raw_dim());
+    let mut k2 = Array::zeros(pq.raw_dim());
+    let mut k3 = Array::zeros(pq.raw_dim());
+    let mut k4 = Array::zeros(pq.raw_dim());
 
     let h2 = h * 0.5;
 
     while tmax >= t {
         println!("{} {} {}", t, pq[0], pq[1]);
-        k1 = ham_eom_1d_harmonic_oscillator(t, &pq);
-        k2 = ham_eom_1d_harmonic_oscillator(t + h2, &(&pq + h2 * &k1));
-        k3 = ham_eom_1d_harmonic_oscillator(t + h2, &(&pq + h2 * &k2));
-        k4 = ham_eom_1d_harmonic_oscillator(t + h, &(&pq + h * &k3));
+        ham_eom_1d_harmonic_oscillator(t, &pq, &mut k1);
+        ham_eom_1d_harmonic_oscillator(t + h2, &(&pq + h2 * &k1), &mut k2);
+        ham_eom_1d_harmonic_oscillator(t + h2, &(&pq + h2 * &k2), &mut k3);
+        ham_eom_1d_harmonic_oscillator(t + h, &(&pq + h * &k3), &mut k4);
 
         pq = &pq + h / 6.0 * (&k1 + 2.0 * &k2 + 2.0 * &k3 + &k4);
 
@@ -36,5 +46,11 @@ fn runge_kutta(h: f32, t0: f32, pq0: Array1<f32>, tmax: f32) {
 }
 
 fn main() {
-    runge_kutta(0.01, 0.0, array![0.0, 1.0], 10.0);
+    let params = IntegrationParams {
+        step_size: 0.01,
+        t0: 0.0,
+        tmax: 10.0,
+    };
+
+    runge_kutta(params, array![0.0, 1.0]);
 }
