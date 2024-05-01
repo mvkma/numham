9e7ec97a3c08093c6c7816b5b41100195fa93e36
Matthias Volk <mv@infrablue.org>
2024-05-01 15:02:34 +0200

    Performance improvements

--- src/physics.rs
+++ src/physics.rs
@@ -53,25 +53,24 @@ impl ThreeBodyHamiltonian {
 
 impl Hamiltonian for ThreeBodyHamiltonian {
     fn eom(&self, _t: f64, pq: ArrayView2<f64>, mut pqdot: ArrayViewMut2<f64>) {
-        let p = pq.index_axis(Axis(0), 0);
         let q = pq.index_axis(Axis(0), 1);
 
-        let r01 = &q.slice(s![0..2]) - &q.slice(s![2..4]);
-        let r02 = &q.slice(s![0..2]) - &q.slice(s![4..6]);
-        let r12 = &q.slice(s![2..4]) - &q.slice(s![4..6]);
+        let mut r01 = &q.slice(s![0..2]) - &q.slice(s![2..4]);
+        let mut r02 = &q.slice(s![0..2]) - &q.slice(s![4..6]);
+        let mut r12 = &q.slice(s![2..4]) - &q.slice(s![4..6]);
 
-        let r01_3 = r01.map(|x| x * x).sum().sqrt().powi(3);
-        let r02_3 = r02.map(|x| x * x).sum().sqrt().powi(3);
-        let r12_3 = r12.map(|x| x * x).sum().sqrt().powi(3);
+        let r01_3 = r01.map(|x| x.powi(2)).sum().sqrt().powi(3);
+        let r02_3 = r02.map(|x| x.powi(2)).sum().sqrt().powi(3);
+        let r12_3 = r12.map(|x| x.powi(2)).sum().sqrt().powi(3);
 
-        let f0 = -self.k * (&r01 / r01_3 + &r02 / r02_3);
-        let f1 = -self.k * (-&r01 / r01_3 + &r12 / r12_3);
-        let f2 = -self.k * (-&r02 / r02_3 - &r12 / r12_3);
+        r01 /= r01_3 * self.k;
+        r02 /= r02_3 * self.k;
+        r12 /= r12_3 * self.k;
 
-        let pdot = &concatenate(Axis(0), &[f0.view(), f1.view(), f2.view()]).unwrap();
-        let qdot = &p / self.m;
+        let pdot = concatenate![Axis(0), -(&r01 + &r02), (&r01 - &r12), (&r02 + &r12)];
+        let qdot = &pq.index_axis(Axis(0), 0) / self.m;
 
-        pqdot.index_axis_mut(Axis(0), 0).assign(pdot);
+        pqdot.index_axis_mut(Axis(0), 0).assign(&pdot);
         pqdot.index_axis_mut(Axis(0), 1).assign(&qdot);
     }
 
